8539babe8894365d23fb7bba31ee5831e2d5bb60
Vera Halaktionova <halaktionova.v@gmail.com>
2023-01-10 13:07:04 +0100

    Add handler event to script.js

---
+++ js/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+function titleClickHandler(event){
+   console.log('Link was clicked!');
+   console.log (event);
+
+  /* remove class 'active' from all article links  */
+
+  /* add class 'active' to the clicked link */
+
+  /* remove class 'active' from all articles */
+
+  /* get 'href' attribute from the clicked link */
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+
+  /* add class 'active' to the correct article */
+}
+
+const links = document.querySelectorAll('.titles a');
+
+for(let link of links){
+   link.addEventListener('click', titleClickHandler);
+}
