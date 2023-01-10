26c2923fd643562e42ba0f250a7ab667054398d2
Vera Halaktionova <halaktionova.v@gmail.com>
2023-01-10 20:27:05 +0100

    Add class active to article

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
 'use strict';
 
 function titleClickHandler(event){
+   const clickedElement = this;
    console.log('Link was clicked!');
    console.log (event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONNE] remove class 'active' from all article links  */
    const activeLinks = document.querySelectorAll('.titles a.active');
 
    for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
    }
 
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+   console.log('clickedElement:', clickedElement);
 
-  /* remove class 'active' from all articles */
+  /* [DONE] remove class 'active' from all articles */
    const activeArticles = document.querySelectorAll('.posts .post.active');
 
    for(let activeArticle of activeArticles){
@@ -25,6 +27,8 @@ function titleClickHandler(event){
   /* find the correct article using the selector (value of 'href' attribute) */
 
   /* add class 'active' to the correct article */
+   clickedElement.classList.add('active');
+
 }
 
 const links = document.querySelectorAll('.titles a');
