4fe0a5b5854ce2c22101200dccf81c121f0a52e3
Vera Halaktionova <halaktionova.v@gmail.com>
2023-01-10 14:27:55 +0100

    Add remove class active from links and articles

--- js/script.js
+++ js/script.js
@@ -5,10 +5,20 @@ function titleClickHandler(event){
    console.log (event);
 
   /* remove class 'active' from all article links  */
+   const activeLinks = document.querySelectorAll('.titles a.active');
+
+   for(let activeLink of activeLinks){
+      activeLink.classList.remove('active');
+   }
 
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
+   const activeArticles = document.querySelectorAll('.posts .post.active');
+
+   for(let activeArticle of activeArticles){
+      activeArticle.classList.remove('active');
+   }
 
   /* get 'href' attribute from the clicked link */
 
