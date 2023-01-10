68b48d9785d1ea085a3ad9c0f87ad1ecc5486791
Vera Halaktionova <halaktionova.v@gmail.com>
2023-01-10 22:34:55 +0100

    Add finding correct article

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 'use strict';
 
 function titleClickHandler(event){
+   
+   event.preventDefault();
+   
    const clickedElement = this;
    console.log('Link was clicked!');
    console.log (event);
@@ -23,8 +26,13 @@ function titleClickHandler(event){
    }
 
   /* get 'href' attribute from the clicked link */
+   const articleSelector = clickedElement.getAttribute('href');
+   console.log(articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
+   const targetArticle = document.querySelector(articleSelector);
+   targetArticle.classList.add('active');
+   console.log(targetArticle);
 
   /* add class 'active' to the correct article */
    clickedElement.classList.add('active');
